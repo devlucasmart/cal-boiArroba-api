57c02ea59a447373b3d3bd70500bfa707aea76fc
Lucas Martins <lucas.martins.fsa@gmail.com>
2023-11-03 08:14:29 -0300

    estruturação do projeto

---
+++ src/main/java/com/devlucasmart/calboiarrobaapi/CalBoiArrobaApiApplication.java
@@ -0,0 +1,13 @@
+package com.devlucasmart.calboiarrobaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalBoiArrobaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalBoiArrobaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devlucasmart/calboiarrobaapi/CalBoiArrobaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlucasmart.calboiarrobaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalBoiArrobaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
